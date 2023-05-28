b9c2efff75edd178b97d7b10a8866708edbca6d3
Simon Le Bras <lebras.simon@gmail.com>
2023-05-28 17:22:06 +0200

    Log jank

--- core/jankstats/src/main/kotlin/app/rickandmorty/jankstats/JankStatsModule.kt
+++ core/jankstats/src/main/kotlin/app/rickandmorty/jankstats/JankStatsModule.kt
@@ -2,19 +2,25 @@ package app.rickandmorty.jankstats
 
 import android.app.Activity
 import androidx.metrics.performance.JankStats
+import app.rickandmorty.logging.Logger
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 
+private const val TAG = "JankStats"
+
 @Module
 @InstallIn(ActivityComponent::class)
 internal object JankStatsModule {
     @Provides
-    fun providesJankStats(activity: Activity): JankStats {
+    fun providesJankStats(
+        activity: Activity,
+        logger: Logger,
+    ): JankStats {
         val frameListener = JankStats.OnFrameListener { frameData ->
             if (frameData.isJank) {
-                // TODO log jank
+                logger.tag(TAG).v(frameData.toString())
             }
         }
         return JankStats.createAndTrack(activity.window, frameListener)
