186b343626762e8d5ddea0c1a612de5c9f549f63
Simon Le Bras <lebras.simon@gmail.com>
2023-05-28 17:21:45 +0200

    Log StrictMode violations

--- core/strictmode/src/main/kotlin/app/rickandmorty/strictmode/StrictModeInitializer.kt
+++ core/strictmode/src/main/kotlin/app/rickandmorty/strictmode/StrictModeInitializer.kt
@@ -7,22 +7,37 @@ import android.os.StrictMode.ThreadPolicy
 import android.os.StrictMode.VmPolicy
 import android.os.strictmode.UntaggedSocketViolation
 import androidx.startup.Initializer
+import app.rickandmorty.logging.Logger
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.components.SingletonComponent
 import java.util.concurrent.Executors
+import javax.inject.Inject
+
+private const val TAG = "StrictMode"
 
 internal class StrictModeInitializer : Initializer<Unit> {
     private val penaltyListenerExecutor by lazy(LazyThreadSafetyMode.NONE) {
         Executors.newSingleThreadExecutor()
     }
 
+    @Inject
+    lateinit var logger: Logger
+
     override fun create(context: Context) {
+        EntryPointAccessors
+            .fromApplication<StrictModeInitializerEntryPoint>(context)
+            .inject(this)
+
         val threadPolicy = ThreadPolicy.Builder()
             .detectAll()
             .apply {
                 if (Build.VERSION.SDK_INT < 28) {
                     penaltyLog()
                 } else {
-                    penaltyListener(penaltyListenerExecutor) {
-                        // TODO log violations
+                    penaltyListener(penaltyListenerExecutor) { violation ->
+                        logger.tag(TAG).w(violation)
                     }
                 }
             }
@@ -43,7 +58,7 @@ internal class StrictModeInitializer : Initializer<Unit> {
                             }
                         }
 
-                        // TODO log violations
+                        logger.tag(TAG).w(violation)
                     }
                 }
             }
@@ -52,4 +67,10 @@ internal class StrictModeInitializer : Initializer<Unit> {
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
+}
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+internal interface StrictModeInitializerEntryPoint {
+    fun inject(initializer: StrictModeInitializer)
 }
