6e7b9d8bf201784014c136e718a098e1d79cd871
Simon Le Bras <lebras.simon@gmail.com>
2023-07-07 08:43:44 +0200

    Update Kotlin to 1.9 (#184)

--- build-logic/src/main/kotlin/app/rickandmorty/gradle/utils/KotlinConfig.kt
+++ build-logic/src/main/kotlin/app/rickandmorty/gradle/utils/KotlinConfig.kt
@@ -4,27 +4,25 @@ import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 internal fun Project.configureKotlin(
     libs: LibrariesForLibs,
-    explicitApi: ExplicitApiMode = ExplicitApiMode.Strict,
+    explicitApi: Boolean = true,
 ) {
     configure<KotlinProjectExtension> {
         val javaVersion = libs.versions.java.get().toInt()
         jvmToolchain(javaVersion)
+
+        if (explicitApi && !name.contains("test", ignoreCase = true)) {
+            explicitApi()
+        }
     }
 
     tasks.withType<KotlinCompile>().configureEach {
         kotlinOptions {
-            freeCompilerArgs += buildList {
-                add("-Xcontext-receivers")
-                if (!name.contains("test", ignoreCase = true)) {
-                    add(explicitApi.toCompilerArg())
-                }
-            }
+            freeCompilerArgs += listOf("-Xcontext-receivers")
         }
     }
 }
