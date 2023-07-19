77b4ee9d04c9ee39fe7d9b04ee75715303fd55e2
Simon Le Bras <lebras.simon@gmail.com>
2023-07-19 09:24:27 +0200

    Use K2 compiler (#201)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://androidx.dev/storage/compose-compiler/repository")
     }
 }
 
