b74eea986ab704379ff830c7985a520c3ae2c05f
Simon Le Bras <lebras.simon@gmail.com>
2024-01-02 19:46:15 +0100

    Remove normalized cache

--- data/graphql-client/src/main/kotlin/app/rickandmorty/data/graphql/client/ApolloClientModule.kt
+++ data/graphql-client/src/main/kotlin/app/rickandmorty/data/graphql/client/ApolloClientModule.kt
@@ -1,27 +1,19 @@
 package app.rickandmorty.data.graphql.client
 
-import android.content.Context
 import app.rickandmorty.core.coroutines.IODispatcher
 import com.apollographql.apollo3.ApolloClient
-import com.apollographql.apollo3.cache.normalized.api.MemoryCacheFactory
-import com.apollographql.apollo3.cache.normalized.api.NormalizedCacheFactory
-import com.apollographql.apollo3.cache.normalized.normalizedCache
-import com.apollographql.apollo3.cache.normalized.sql.SqlNormalizedCacheFactory
 import com.apollographql.apollo3.network.http.DefaultHttpEngine
 import com.apollographql.apollo3.network.http.HttpEngine
 import dagger.Lazy
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineDispatcher
 import okhttp3.OkHttpClient
 
 private const val SERVER_URL = "https://rickandmortyapi.com/graphql"
-
-private const val MEMORY_CACHE_SIZE = 10 * 1024 * 1024 // 10 MB
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -33,25 +25,13 @@ internal class ApolloClientModule {
 
     @Provides
     @Singleton
-    fun provideNormalizedCacheFactory(
-        @ApplicationContext context: Context,
-    ): NormalizedCacheFactory {
-        return MemoryCacheFactory(maxSizeBytes = MEMORY_CACHE_SIZE).chain(
-            SqlNormalizedCacheFactory(context),
-        )
-    }
-
-    @Provides
-    @Singleton
     fun provideApolloClient(
         httpEngine: HttpEngine,
-        cacheFactory: NormalizedCacheFactory,
         @IODispatcher ioDispatcher: CoroutineDispatcher,
     ): ApolloClient {
         return ApolloClient.Builder()
             .serverUrl(SERVER_URL)
             .httpEngine(httpEngine)
-            .normalizedCache(cacheFactory)
             .dispatcher(ioDispatcher)
             .enableAutoPersistedQueries(true)
             .build()
