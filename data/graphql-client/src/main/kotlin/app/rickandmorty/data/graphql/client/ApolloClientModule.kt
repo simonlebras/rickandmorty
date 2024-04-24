df31a2dfced91db05643bba8ce9e542a3b703395
Simon Le Bras <lebras.simon@gmail.com>
2024-04-24 15:25:05 +0200

    Add network monitor to reduce latency

--- data/graphql-client/src/main/kotlin/app/rickandmorty/data/graphql/client/ApolloClientModule.kt
+++ data/graphql-client/src/main/kotlin/app/rickandmorty/data/graphql/client/ApolloClientModule.kt
@@ -1,12 +1,16 @@
 package app.rickandmorty.data.graphql.client
 
+import android.content.Context
 import app.rickandmorty.core.coroutines.IODispatcher
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.annotations.ApolloExperimental
+import com.apollographql.apollo3.network.NetworkMonitor
 import com.apollographql.apollo3.network.okHttpCallFactory
 import dagger.Lazy
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineDispatcher
@@ -14,17 +18,26 @@ import okhttp3.OkHttpClient
 
 private const val SERVER_URL = "https://rickandmortyapi.com/graphql"
 
+@OptIn(ApolloExperimental::class)
 @Module
 @InstallIn(SingletonComponent::class)
 internal class ApolloClientModule {
     @Provides
+    fun provideNetworkMonitor(
+        @ApplicationContext context: Context,
+    ): NetworkMonitor = NetworkMonitor(context)
+
+    @Provides
     @Singleton
     fun provideApolloClient(
+        networkMonitor: NetworkMonitor,
         okHttpClient: Lazy<OkHttpClient>,
         @IODispatcher ioDispatcher: CoroutineDispatcher,
     ): ApolloClient {
         return ApolloClient.Builder()
             .serverUrl(SERVER_URL)
+            .networkMonitor(networkMonitor)
+            .failFastIfOffline(true)
             .okHttpCallFactory { okHttpClient.get() }
             .dispatcher(ioDispatcher)
             .enableAutoPersistedQueries(true)
