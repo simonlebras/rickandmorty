f6cee544a5bd266708f0bc3b14092677e149d7bf
Simon Le Bras <lebras.simon@gmail.com>
2024-04-30 10:27:27 +0200

    Move enableEdgeToEdge out of Compose

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -9,6 +9,7 @@ android {
 }
 
 dependencies {
+    api(libs.androidx.activity)
     api(libs.androidx.compose.foundation.layout)
     api(libs.androidx.paging.compose)
     api(libs.androidx.window)
