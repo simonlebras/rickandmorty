00b726cc14ba880e4a0fc8022458d286d89d33bf
Simon Le Bras <lebras.simon@gmail.com>
2024-05-02 11:07:01 +0200

    Update Compose BOM to 2024.05.00-alpha01

--- core/design-system/src/main/kotlin/app/rickandmorty/core/designsystem/component/PullToRefresh.kt
+++ core/design-system/src/main/kotlin/app/rickandmorty/core/designsystem/component/PullToRefresh.kt
@@ -5,14 +5,13 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
 import androidx.compose.material3.pulltorefresh.PullToRefreshState
+import androidx.compose.material3.pulltorefresh.pullToRefresh
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,23 +24,18 @@ public fun PullToRefresh(
     indicatorPadding: PaddingValues = PaddingValues(0.dp),
     content: @Composable BoxScope.() -> Unit,
 ) {
-    if (state.isRefreshing) {
-        LaunchedEffect(onRefresh) {
-            onRefresh()
-        }
-    }
-
-    if (!isRefreshing && state.isRefreshing) {
-        LaunchedEffect(state) {
-            state.endRefresh()
-        }
-    }
-
-    Box(modifier = modifier.nestedScroll(connection = state.nestedScrollConnection)) {
+    Box(
+        modifier = modifier.pullToRefresh(
+            state = state,
+            isRefreshing = isRefreshing,
+            onRefresh = onRefresh,
+        ),
+    ) {
         content()
 
-        PullToRefreshContainer(
+        Indicator(
             state = state,
+            isRefreshing = isRefreshing,
             modifier = Modifier
                 .align(Alignment.TopCenter)
                 .padding(indicatorPadding),
