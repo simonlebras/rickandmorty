b15380dc2570a8743f1d7801c3b88a2d219ddf3e
Simon Le Bras <slebras@deezer.com>
2024-08-01 00:10:41 +0200

    Add Android 15 compatibility

--- features/settings/src/main/kotlin/app/rickandmorty/feature/settings/util/ContextExt.kt
+++ features/settings/src/main/kotlin/app/rickandmorty/feature/settings/util/ContextExt.kt
@@ -3,4 +3,4 @@ package app.rickandmorty.feature.settings.util
 import android.content.Context
 
 internal val Context.versionName: String
-    get() = packageManager.getPackageInfo(packageName, 0).versionName
+    get() = packageManager.getPackageInfo(packageName, 0).versionName!!
