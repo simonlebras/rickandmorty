bcb58b21fb83091dff8cec088ece831e717f6a12
Simon Le Bras <lebras.simon@gmail.com>
2024-09-09 20:28:16 +0200

    Fix transparent TopAppBars

--- features/characters/src/main/kotlin/app/rickandmorty/feature/characters/list/CharacterListScreen.kt
+++ features/characters/src/main/kotlin/app/rickandmorty/feature/characters/list/CharacterListScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -230,10 +229,6 @@ private fun CharacterListScreenAppBar(
                 )
             }
         },
-        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-            containerColor = Color.Transparent,
-            scrolledContainerColor = Color.Transparent,
-        ),
         scrollBehavior = scrollBehavior,
     )
 }
