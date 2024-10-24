bfed1624baac91bbba22c6a76701e08981f38c1b
Simon Le Bras <lebras.simon@gmail.com>
2024-10-24 10:01:04 +0200

    Migrate Startup to KMP (#931)

--- core/startup/build.gradle.kts
+++ core/startup/build.gradle.kts
@@ -1,13 +1,7 @@
 plugins {
-    alias(libs.plugins.rickandmorty.android.library)
-    alias(libs.plugins.rickandmorty.hilt)
-    alias(libs.plugins.rickandmorty.kotlin.android)
+    alias(libs.plugins.rickandmorty.kotlin.multiplatform)
     alias(libs.plugins.rickandmorty.spotless)
 
     alias(libs.plugins.dependencyanalysis)
     alias(libs.plugins.sortdependencies)
-}
-
-android {
-    namespace = "app.rickandmorty.core.startup"
 }
