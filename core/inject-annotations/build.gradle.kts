a67744cb0c730c2f7b8af604cf890cef261db3a7
Simon Le Bras <lebras.simon@gmail.com>
2025-02-13 08:34:18 +0100

    Move AppContext to androidMain (#1116)

--- core/inject-annotations/build.gradle.kts
+++ core/inject-annotations/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
+    alias(libs.plugins.rickandmorty.android.library)
     alias(libs.plugins.rickandmorty.kotlin.multiplatform)
     alias(libs.plugins.rickandmorty.spotless)
 
     alias(libs.plugins.dependencyanalysis)
     alias(libs.plugins.sortdependencies)
+}
+
+android {
+    namespace = "app.rickandmorty.core.injectannotations"
 }
 
 kotlin {
