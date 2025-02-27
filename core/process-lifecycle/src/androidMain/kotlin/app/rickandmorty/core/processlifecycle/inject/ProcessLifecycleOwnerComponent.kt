6039967a8a0fb61be70abc4893ed0d8f87308195
Simon Le Bras <lebras.simon@gmail.com>
2025-02-27 09:10:05 +0100

    Inject ProcessLifecycleOwner (#1139)

---
+++ core/process-lifecycle/src/androidMain/kotlin/app/rickandmorty/core/processlifecycle/inject/ProcessLifecycleOwnerComponent.kt
@@ -0,0 +1,14 @@
+package app.rickandmorty.core.processlifecycle.inject
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner as AndroidxProcessLifecycleOwner
+import me.tatarka.inject.annotations.Provides
+import software.amazon.lastmile.kotlin.inject.anvil.AppScope
+import software.amazon.lastmile.kotlin.inject.anvil.ContributesTo
+
+@ContributesTo(AppScope::class)
+public interface ProcessLifecycleOwnerComponent {
+    @Provides
+    @ProcessLifecycleOwner
+    public fun provideProcessLifecycleOwner(): LifecycleOwner = AndroidxProcessLifecycleOwner.get()
+}
