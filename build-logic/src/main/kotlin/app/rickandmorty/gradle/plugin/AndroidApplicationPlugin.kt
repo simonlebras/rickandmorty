624347d72d922df136d9cb9d4ce44cdd975b5353
Simon Le Bras <lebras.simon@gmail.com>
2025-03-12 09:54:48 +0100

    Improve packaging exclusions (#1159)

--- build-logic/src/main/kotlin/app/rickandmorty/gradle/plugin/AndroidApplicationPlugin.kt
+++ build-logic/src/main/kotlin/app/rickandmorty/gradle/plugin/AndroidApplicationPlugin.kt
@@ -5,11 +5,13 @@ import app.rickandmorty.gradle.util.configureAffectedAndroidTest
 import app.rickandmorty.gradle.util.configureAndroid
 import app.rickandmorty.gradle.util.configureBadgingTasks
 import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.variant.ApplicationAndroidComponentsExtension
 import com.android.build.gradle.AppExtension
 import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.the
 
 public class AndroidApplicationPlugin : Plugin<Project> {
@@ -31,15 +33,37 @@ public class AndroidApplicationPlugin : Plugin<Project> {
 
             defaultConfig.targetSdk = libs.versions.android.sdk.target.get().toInt()
 
-            packaging {
-                resources {
-                    excludes += "/META-INF/{AL2.0,LGPL2.1}"
-                }
-            }
+            packaging.resources.excludes += setOf(
+                "**/*.pro",
+                "**/*.proto",
+                "**/*.properties",
+                "*.properties",
+                "LICENSE_*",
+                "/META-INF/{AL2.0,LGPL2.1}",
+                "META-INF/CHANGES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+            )
         }
 
-        configure<AppExtension> {
-            configureBadgingTasks(this)
+        configureBadgingTasks(
+            appExtension = extensions.getByType<AppExtension>(),
+            componentsExtension = extensions.getByType<ApplicationAndroidComponentsExtension>(),
+        )
+
+        configure<ApplicationAndroidComponentsExtension> {
+            onVariants(selector().withBuildType("release")) { variant ->
+                variant.packaging.resources.excludes.addAll(
+                    "**/*.kotlin_metadata",
+                    "DebugProbesKt.bin",
+                    "kotlin/**",
+                    "META-INF/*.kotlin_module",
+                    "META-INF/*.version",
+                    "META-INF/androidx.*",
+                )
+            }
         }
     }
 }
