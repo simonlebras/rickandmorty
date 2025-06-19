8468f67f3deb7c7d4871ce24cbf7b1c98a89fd3c
Simon Le Bras <lebras.simon@gmail.com>
2025-06-19 19:56:15 +0200

    Migrate Apollo to KMP

---
+++ data/graphql-client/src/commonMain/kotlin/app/rickandmorty/data/graphql/client/ApolloClientProvider.kt
@@ -0,0 +1,37 @@
+package app.rickandmorty.data.graphql.client
+
+import app.rickandmorty.core.coroutines.inject.IODispatcher
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.annotations.ApolloExperimental
+import com.apollographql.apollo.interceptor.RetryOnErrorInterceptor
+import com.apollographql.apollo.network.NetworkMonitor
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.Provides
+import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.CoroutineDispatcher
+
+private const val SERVER_URL = "https://rickandmortyapi.com/graphql"
+
+@OptIn(ApolloExperimental::class)
+@ContributesTo(AppScope::class)
+public interface ApolloClientProvider {
+  public companion object {
+    @Provides
+    public fun provideApolloClient(
+      @IODispatcher ioDispatcher: CoroutineContext,
+      networkMonitor: NetworkMonitor? = null,
+    ): ApolloClient =
+      ApolloClient.Builder()
+        .serverUrl(SERVER_URL)
+        .apply {
+          networkMonitor?.let { monitor ->
+            retryOnErrorInterceptor(RetryOnErrorInterceptor(monitor))
+          }
+        }
+        .failFastIfOffline(true)
+        .dispatcher(ioDispatcher[CoroutineDispatcher])
+        .enableAutoPersistedQueries(true)
+        .build()
+  }
+}
---
+++ data/graphql-client/src/nativeMain/kotlin/app/rickandmorty/data/graphql/client/NetworkMonitorProvider.kt
@@ -0,0 +1,15 @@
+package app.rickandmorty.data.graphql.client
+
+import com.apollographql.apollo.annotations.ApolloExperimental
+import com.apollographql.apollo.network.NetworkMonitor
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.Provides
+
+@OptIn(ApolloExperimental::class)
+@ContributesTo(AppScope::class)
+public interface NetworkMonitorProvider {
+  public companion object {
+    @Provides public fun provideNetworkMonitor(): NetworkMonitor = NetworkMonitor()
+  }
+}
