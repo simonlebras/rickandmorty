05589b74a9b776de89c368e253b1cb637180060b
Simon Le Bras <lebras.simon@gmail.com>
2025-10-23 22:08:33 +0200

    Add ColorImagePreviewHandler (#1624)

--- core/ui-tooling-preview/build.gradle.kts
+++ core/ui-tooling-preview/build.gradle.kts
@@ -1,15 +1,34 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
 plugins {
-  alias(libs.plugins.rickandmorty.android.library)
+  alias(libs.plugins.rickandmorty.android.multiplatformlibrary)
+  alias(libs.plugins.rickandmorty.compose)
+  alias(libs.plugins.rickandmorty.kotlin.multiplatform)
 }
 
-android {
-  namespace = "app.rickandmorty.core.ui.tooling.preview"
-}
+kotlin {
+  android { namespace = "app.rickandmorty.core.ui.tooling.preview" }
 
-dependencies {
+  @OptIn(ExperimentalKotlinGradlePluginApi::class)
+  dependencies {
     api(libs.jetbrains.compose.ui.tooling.preview)
 
-    implementation(project(":core:resources-app"))
+    implementation(libs.coil.compose)
 
-    implementation(libs.jetbrains.compose.ui.tooling)
+    implementation(libs.jetbrains.compose.material3)
+  }
+
+  sourceSets {
+    androidMain {
+      dependencies {
+        api(libs.jetbrains.compose.ui.tooling)
+
+        api(libs.androidx.activity.compose)
+        api(libs.androidx.customview.poolingcontainer)
+        api(libs.androidx.emoji2)
+
+        implementation(project(":core:resources-app"))
+      }
+    }
+  }
 }
