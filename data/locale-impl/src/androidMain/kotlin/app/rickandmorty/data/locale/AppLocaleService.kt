cb497138f91dab0cb0e03aae58def6cb2f374986
Simon Le Bras <lebras.simon@gmail.com>
2026-01-03 18:29:19 +0100

    Simplify locale management implementation

--- data/locale-impl/src/androidMain/kotlin/app/rickandmorty/data/locale/AppLocaleService.kt
+++
@@ -1,9 +0,0 @@
-package app.rickandmorty.data.locale
-
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
-
-internal class AppLocaleService : Service() {
-  override fun onBind(intent: Intent): IBinder = throw UnsupportedOperationException()
-}
--- data/locale-impl/src/commonMain/kotlin/app/rickandmorty/data/locale/LocaleRepositoryImpl.kt
+++ data/locale-impl/src/commonMain/kotlin/app/rickandmorty/data/locale/LocaleRepositoryImpl.kt
@@ -1,11 +1,8 @@
 package app.rickandmorty.data.locale
 
-import androidx.datastore.core.DataStore
 import app.rickandmorty.core.coroutines.inject.ApplicationScope
-import app.rickandmorty.core.coroutines.inject.IODispatcher
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesBinding
-import kotlin.coroutines.CoroutineContext
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
@@ -14,22 +11,14 @@ import kotlinx.coroutines.launch
 @ContributesBinding(AppScope::class)
 public class LocaleRepositoryImpl(
   private val platformLocaleManager: PlatformLocaleManager,
-  private val dataStore: DataStore<LocaleProto>,
   @ApplicationScope private val applicationScope: CoroutineScope,
-  @IODispatcher private val ioDispatcher: CoroutineContext,
 ) : LocaleRepository {
   override fun getAppLocale(): Flow<Locale?> {
     return platformLocaleManager.getAppLocale()
   }
 
   override suspend fun setAppLocale(locale: Locale?) {
-    applicationScope
-      .launch {
-        dataStore.updateData { preferences ->
-          preferences.copy(appLocale = locale?.toLanguageTag())
-        }
-      }
-      .join()
+    applicationScope.launch { platformLocaleManager.setAppLocale(locale) }.join()
   }
 
   override suspend fun getAvailableAppLocales(): ImmutableList<Locale> {
