b95b783119aed177fd6ea08fada4309b2cc023d7
Simon Le Bras <lebras.simon@gmail.com>
2026-02-25 08:16:04 +0100

    Remove unused bindings

--- app/src/main/kotlin/app/rickandmorty/inject/UiGraph.kt
+++ app/src/main/kotlin/app/rickandmorty/inject/UiGraph.kt
@@ -1,16 +1,11 @@
 package app.rickandmorty.inject
 
-import android.app.Activity
-import android.content.Context
 import androidx.savedstate.serialization.SavedStateConfiguration
-import app.rickandmorty.core.metro.ActivityContext
 import app.rickandmorty.core.metro.UiScope
 import app.rickandmorty.core.navigation.EntryProvider
 import dev.zacsweers.metro.AppScope
-import dev.zacsweers.metro.Binds
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.GraphExtension
-import dev.zacsweers.metro.Provides
 import dev.zacsweers.metrox.viewmodel.MetroViewModelFactory
 import dev.zacsweers.metrox.viewmodel.ViewModelGraph
 
@@ -22,11 +17,9 @@ interface UiGraph : ViewModelGraph {
 
   val savedStateConfiguration: SavedStateConfiguration
 
-  @Binds @ActivityContext fun bindActivity(activity: Activity): Context
-
   @GraphExtension.Factory
   @ContributesTo(AppScope::class)
   fun interface Factory {
-    fun create(@Provides activity: Activity): UiGraph
+    fun create(): UiGraph
   }
 }
